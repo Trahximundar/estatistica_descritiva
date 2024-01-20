34c3654395f17c1ae1eb3f2e20d125e776562ecf
Trahximundar <evertonbarbosapeixoto@gmail.com>
2024-01-20 19:35:10 -0300

    Primeiro teste.

---
+++ test_git.R
@@ -0,0 +1,152 @@
+rm(list = ls())
+
+install.packages("patchwork")
+install.packages("summarytools")
+install.packages("credentials")
+
+library(credentials)
+library(summarytools)
+library(patchwork)
+library(ggspatial)
+library(RColorBrewer)
+library(geobr)
+library(janitor)
+library(sf)
+library(ggplot2)
+library(tidyverse)
+library(tibble)
+library(readxl)
+library(dplyr)
+
+####################################################################################################################################################################################
+
+setwd("C:/Users/Flávia Cristina/Documents/pos/estatistica/Dados Metereologicos/")
+
+##### Distribuião de Frequência ###############################################################################################################################################################################
+
+#Dados
+dados <- read.csv2("Dados Metereologicos.csv") %>% 
+  janitor::clean_names()
+
+glimpse(dados)
+
+#Gráficos Individuais
+#Freq. Absoluta
+ggplot(dados,aes(x=precipitacao_mm))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Absoluta",title = "Histograma")+
+  theme_minimal()
+
+#Freq. Relativa
+ggplot(dados,aes(x = precipitacao_mm, y = (..count../sum(..count..))*100))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Relativa",title = "Histograma")+
+  theme_minimal()
+
+#Freq. Acumulada
+ggplot(dados,aes(x=precipitacao_mm,cumsum(..count..)))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Acumulada",title = "Histograma")+
+  theme_minimal()
+
+#Freq. Relativa Acumulada
+ggplot(dados,aes(x=precipitacao_mm, y = cumsum((..count../sum(..count..))*100)))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Relativa Acumulada(%)",title = "Histograma")+
+  theme_minimal()
+
+#Gráficos Juntos
+#Freq. Absoluta
+g1 <- ggplot(dados,aes(x=precipitacao_mm))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Absoluta")+
+  theme_minimal()
+
+#Freq. Relativa
+g2 <- ggplot(dados,aes(x = precipitacao_mm, y = (..count../sum(..count..))*100))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Relativa")+
+  theme_minimal()
+
+#Freq. Acumulada
+g3 <- ggplot(dados,aes(x=precipitacao_mm,cumsum(..count..)))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Acumulada")+
+  theme_minimal()
+
+#Freq. Relativa Acumulada
+g4 <- ggplot(dados,aes(x=precipitacao_mm, y = cumsum((..count../sum(..count..))*100)))+
+  geom_histogram(bins = 12,
+                 col="blue",
+                 fill="lightblue")+
+  labs(x="Precipitação Média (mm)",y = "Frequência Relativa Acumulada(%)")+
+  theme_minimal()
+
+(g1 + g2)/(g3 + g4) + plot_annotation(title = "Estatistica Descritiva",
+                                      subtitle = "Nome: BELO HORIZONTE - CERCADINHO
+                                                  Codigo Estacao: F501
+                                                  Latitude: -19.97999999
+                                                  Longitude: -43.95861111
+                                                  Altitude: 1199.55
+                                                  Situacao: Operante
+                                                  Data Inicial: 2020-01-01
+                                                  Data Final: 2020-01-31
+                                                  Periodicidade da Medicao: Diaria",
+                                      tag_levels = c("A","B","C","D"),
+                                      tag_prefix = "Gráfico ")
+
+##### Tabela de Distribuião  de Frequência ###############################################################################################################################################################################
+
+dados.cat <- cut(dados$precipitacao_mm, breaks = c(0,57,114,180),
+                 right = F,
+                 labels = c("0-57","57-114","114-180")) 
+
+summarytools::freq(dados.cat,report.nas = FALSE,
+                   style = "rmarkdown")
+
+##### XXXXXX  ###############################################################################################################################################################################
+
+
+
+
+
+
+
+R.version.string
+
+usethis::git_default_branch_configure('principal')
+gitcreds::gitcreds_set()
+
+usethis::create_github_token()
+
+usethis::create_github_token()
+
+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
+
+gitcreds::gitcreds_set()
+
+gitcreds::gitcreds_set()
+1
+
+
+usethis::edit_r_environ()
+
+usethis::git_sitrep()
+
+usethis::use_git()
+3
+
